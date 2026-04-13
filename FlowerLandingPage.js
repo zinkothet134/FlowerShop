6f29266b7433fe9b7c8ea9745eb31eb5ad9fef54
Zin Ko Thet <zinkothet153586@gmail.com>
2026-04-13 14:15:55 +0700

    intitial commit

--- FlowerLandingPage.js
+++ FlowerLandingPage.js
@@ -8,7 +8,72 @@ export default function FlowerlandingPage() {
       <View style={styles.header}>
         <Text style={styles.headerText}>Blooming Flowers</Text>
         <Text style={styles.subHeaderText}>Fresh and Beautiful Flowers Delivered to Your Doorstep</Text>
-      </View>      
+      </View>   
+      <View style={styles.imageContainer}>
+        <Image source={{ uri : 'https://images.unsplash.com/photo-1463043254199-7a3efd782ad1?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}} style={styles.image}></Image>
+      </View>   
+      <View style={styles.searchContainer}>
+        <TextInput
+        style={styles.TextInput}
+        placeholder='Search for flowers...'
+        ></TextInput>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.featuredSection}>
+          <Text style={styles.sectionTitle}></Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.flowerCard}>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1686125616977-34f6d5979eb1?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={styles.flowerImage}
+            />
+            <Text style={styles.flowerTitle}>White Base</Text>
+          </View>
+          <View style={styles.flowerCard}>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1581264692636-3cf6f29655c2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={styles.flowerImage}
+            />
+            <Text style={styles.flowerTitle}>Red Rose</Text>
+          </View>
+          <View style={styles.flowerCard}>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1579847621515-b40fcc20831e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={styles.flowerImage}
+            />
+            <Text style={styles.flowerTitle}>Lily</Text>
+          </View>
+          <View style={styles.flowerCard}>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1487435636644-3ad040f0195b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={styles.flowerImage}
+            />
+            <Text style={styles.flowerTitle}>Tulip</Text>
+          </View>
+          <View style={styles.flowerCard}>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1556216750-2108e1e54e9a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={styles.flowerImage}
+            />
+            <Text style={styles.flowerTitle}>Orchid</Text>
+          </View>
+          <View style={styles.flowerCard}>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1695112691738-5227cabb206f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={styles.flowerImage}
+            />
+            <Text style={styles.flowerTitle}>Chrysanthemums</Text>
+          </View>
+          </ScrollView>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={()=> alert('Shop Now!')}>
+              <Text style={styles.buttonText}>Show Now</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </View>
     </ScrollView>
   );
 }
@@ -47,9 +112,9 @@ const styles = StyleSheet.create({
     },
     textInput: {
       height: 90,
-      borderColor: '#ddd',
+      borderColor: '#8b5d5d',
       borderWidth: 1,
-      borderRadius: 5,
+      borderRadius: 20,
       paddingHorizontal: 10,
       fontSize:25
     },
